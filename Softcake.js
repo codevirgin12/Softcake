0f0c9e8eab6d718ec8d8bcc88c341d3e030b44c6
Frøya <froyagj@gmail.com>
2021-09-09 13:15:53 +0200

    Softcake

--- Softcake.js
+++ Softcake.js
@@ -1,3 +1,8 @@
 let egg = 5/16*5;
 console.log('Egg:' + egg);
-    
+let sukker = 150/16*150;
+console.log('Sukker:' + sukker);
+let hvetemel = 150/16*150;
+console.log('Hvetemel:' + hvetemel);
+let bakepulver = 1/16*5;
+console.log('Bakepulver:' + bakepulver);
