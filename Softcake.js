04f85656c5fb025ffaf6c940755256d6bae6e5ee
Frøya <froyagj@gmail.com>
2021-09-10 09:22:47 +0200

    Softcake

--- Softcake.js
+++ Softcake.js
@@ -10,11 +10,19 @@ console.log('Hvetemel:' + hvetemel);
 let bakepulver = 1/16*5;
 console.log('Bakepulver:' + bakepulver);
 
-console.log(Fyll);
+console.log(Fyll og pynt);
 let kremfløte = 3/16*5;
 console.log('Kremfløte:' + kremfløte);
 let melis = 2/16*5;
 console.log('Melis:' + melis);
-let melk = 1/16*5
+let melk = 1/16*5;
 console.log('Melk:' + melk);
+let jordbærsyltetøy = 8/16*5
+console.log('Jordbærsyltetøy:' + jordbærsyltetøy);
+let Valnøttkjerner = 50/16*5;
+console.log('Valnøttkjerner:' + valnøttkjerner);
+let banan = 1/16*5;
+console.log('Banan:' + banan);
+let jordbær = 2/16*5
+console.log('Jordbær:' + jordbær);
 
